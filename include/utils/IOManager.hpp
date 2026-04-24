1555f0d4bcf30aaebdb1703dbba253db699db016
SukaBotol <adharmayudha@gmail.com>
2026-04-24 07:07:59 +0700

    feet: config loader

--- include/utils/IOManager.hpp
+++ include/utils/IOManager.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <string>
-#include "SaveData.hpp" 
-#include "ConfigData.hpp"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include "../../include/models/PropertyTile.hpp"
+#include "../../include/models/StreetTile.hpp"
+#include "../../include/models/Tile.hpp"
+#include "../../include/models/Board.hpp" 
+#include "../../include/utils/SaveData.hpp" 
+#include "../../include/utils/ConfigData.hpp"
 
 class IOManager {
 public:
@@ -11,9 +18,13 @@ public:
 
     GameSaveData loadGameData(const std::string& filepath);
     void saveGameData(const GameSaveData& data, const std::string& filepath);
-    FullConfigData loadAllConfigs(const std::string& configDirectoryPath);
-
-private:
+    static void loadAllConfigs(const std::string& configDirectoryPath);
+    
+    private:
+    std::vector<std::unique_ptr<Tile>> IOparsePropertyTileConfig(const std::string& configDirectoryPath);
+    std::vector<std::unique_ptr<Tile>> IOparseActionTileConfig(const std::string& configDirectoryPath, std::vector<int>& tax, std::vector<int>& special);
+    std::vector<int> IOparseTaxConfig(const std::string& configDirectoryPath);
+    std::vector<int> IOparseSpecialConfig(const std::string& configDirectoryPath);
     void parseMiscConfig(const std::string& filepath, FullConfigData& config);
     void parseTaxConfig(const std::string& filepath, FullConfigData& config);
     void parseSpecialConfig(const std::string& filepath, FullConfigData& config);
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "../include/utils/IOManager.hpp"
+
+// untuk test doang 
+int main(){
+    IOManager::loadAllConfigs("./config");
+
+    return 0;
+}
