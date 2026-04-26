067be61e1239bd90c0587388e2151251de4a70c0
billie-bytes <billiebaskarawibawa101@gmail.com>
2026-04-26 19:38:11 +0700

    fixed card getName

--- src/models/Card.cpp
+++ src/models/Card.cpp
@@ -1,6 +1,9 @@
 #include "../../include/models/Card.hpp"
 
 Card::Card(std::string name):name(name){}
+std::string Card::getName(){
+    return name;
+}
 
 std::random_device Card::rd;
 std::mt19937 Card::gen(Card::rd());
