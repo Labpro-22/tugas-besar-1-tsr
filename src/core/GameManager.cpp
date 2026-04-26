cdc459bdb83fecd7f66c10fe921b60f28fdc7bf9
billie-bytes <billiebaskarawibawa101@gmail.com>
2026-04-26 17:34:23 +0700

    Turn order saved

--- src/core/GameManager.cpp
+++ src/core/GameManager.cpp
@@ -71,10 +71,22 @@ void GameManager::visitUtilityTile(UtilityTile* tile, Player& player) {
 
 std::string GameManager::toSaveFormat() const{
     std::ostringstream out;
+    // <TURN_SAAT_INI> <MAX_TURN>
     out << current_turn_count << " " << max_turns << "\n";
+    // <JUMLAH_PEMAIN>
     out << players.size();
+
+    //<STATE_PEMAIN_1..N>
     for(const std::shared_ptr<Player>& p:players){
         out << p->toSaveFormat();
     }
+    //<URUTAN_GILIRAN_1> <URUTAN_GILIRAN_2> … <URUTAN_GILIRAN_N>
+    for(int i = 0; i<current_turn_count; i++){
+        if(i!=current_turn_count-1) out << players[i]->getname() << " ";
+        else out << players[i]->getname() << "\n";
+    }
+    //<GILIRAN_AKTIF_SAAT_INI>
+    out << players[players.size()%current_turn_count]->getname() << "\n";
+
     return out.str();
 }
